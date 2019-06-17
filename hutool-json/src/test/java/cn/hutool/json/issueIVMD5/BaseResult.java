043b9b791b10f5041b31ad81ead8d3afeb8d6338
wuwl <449720689@qq.com>
2019-06-17 16:56:27 +0800

    Update BaseResult.java
    Co-Authored-By: wuwl <rosennevergiveup@users.noreply.github.com>

--- hutool-json/src/test/java/cn/hutool/json/issueIVMD5/BaseResult.java
+++ hutool-json/src/test/java/cn/hutool/json/issueIVMD5/BaseResult.java
@@ -16,5 +16,41 @@ public class BaseResult<E> {
 	private String nextDataUri;
 	private String message;
 	private int dataCount;
+	public int getResult() {
+		return result;
+	}
+	public void setResult(int result) {
+		this.result = result;
+	}
+	public List<E> getData() {
+		return data;
+	}
+	public void setData(List<E> data) {
+		this.data = data;
+	}
+	public E getData2() {
+		return data2;
+	}
+	public void setData2(E data2) {
+		this.data2 = data2;
+	}
+	public String getNextDataUri() {
+		return nextDataUri;
+	}
+	public void setNextDataUri(String nextDataUri) {
+		this.nextDataUri = nextDataUri;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getDataCount() {
+		return dataCount;
+	}
+	public void setDataCount(int dataCount) {
+		this.dataCount = dataCount;
+	}
 	
 }
